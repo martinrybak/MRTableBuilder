c27752b2c1dad31e1197944eac47cff5d8e7edf3
Mark Rogers <mark.rogers@yodle.com>
2015-07-06 15:32:00 -0400

    Revert "Fixed auto layout bug that affects cells containing multiline labels."
    This reverts commit 0ad515281ee18772c99d5989a41eae370bc47184.

--- Pod/Classes/MRTableSection.m
+++ Pod/Classes/MRTableSection.m
@@ -51,24 +51,6 @@
 	[self.tableBuilder.tableView endUpdates];
 }
 
-- (void)addRows:(NSArray*)rows
-{
-	for (MRTableRow* row in rows) {
-		[self addRow:row];
-	}
-}
-
-- (void)addRows:(NSArray*)rows withAnimation:(UITableViewRowAnimation)animation
-{
-	[self.tableBuilder.tableView beginUpdates];
-	for (MRTableRow* row in rows) {
-		[self addRow:row];
-		NSIndexPath* indexPath = [self.tableBuilder indexPathForRow:row];
-		[self.tableBuilder.tableView insertRowsAtIndexPaths:@[ indexPath ] withRowAnimation:animation];
-	}
-	[self.tableBuilder.tableView endUpdates];
-}
-
 - (void)removeRow:(MRTableRow*)row
 {
 	row.section = nil;
@@ -230,7 +212,6 @@
 	
 	//Calculate cell height using auto layout
 	UITableViewCell* cell = [row buildCell];
-	[cell layoutIfNeeded];
 	CGFloat cellHeight = [cell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
 	
 	//No height, default to tableView row height
