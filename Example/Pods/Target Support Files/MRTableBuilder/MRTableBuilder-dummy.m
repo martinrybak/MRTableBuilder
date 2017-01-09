0110b978ed5ef887b068e0f0b106fc806e66baaa
Martin Rybak <martin.rybak@gmail.com>
2017-01-09 16:09:39 -0500

    Updated project to iOS 10 settings and updated Cocoapods

---
+++ Example/Pods/Target Support Files/MRTableBuilder/MRTableBuilder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MRTableBuilder : NSObject
+@end
+@implementation PodsDummy_MRTableBuilder
+@end
--- Example/Pods/Target Support Files/Pods-MRTableBuilder-MRTableBuilder/Pods-MRTableBuilder-MRTableBuilder-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MRTableBuilder_MRTableBuilder : NSObject
-@end
-@implementation PodsDummy_Pods_MRTableBuilder_MRTableBuilder
-@end
